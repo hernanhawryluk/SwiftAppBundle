52f0a63195dbcbdda6cf4eba791d963457015126
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-08-15 18:55:41 -0300

    Initial Commit

---
+++ SwiftAppBundle/SwiftAppBundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppBundleApp.swift
+//  SwiftAppBundle
+//
+//  Created by Hernan Hawryluk on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppBundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
