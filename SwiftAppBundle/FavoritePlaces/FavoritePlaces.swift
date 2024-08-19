0ae78417b5142c45009ff9ad86fae62ed79aff14
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-08-19 11:13:00 -0300

    Added a bottom sheet and data persistence

--- SwiftAppBundle/FavoritePlaces/FavoritePlaces.swift
+++ SwiftAppBundle/FavoritePlaces/FavoritePlaces.swift
@@ -23,7 +23,9 @@ struct FavoritePlaces: View {
     @State var name: String = ""
     @State var favorite: Bool = false
     @State var error: Bool = false
+    @State var showSheet = false
     
+    let height = stride(from: 0.15, through: 0.15, by: 0.1).map { PresentationDetent.fraction(($0))}
     var body: some View {
         ZStack {
             MapReader { proxy in
@@ -41,11 +43,24 @@ struct FavoritePlaces: View {
                                 .frame(width: 12)
                         }
                     }
-                }                    .onTapGesture { coord in
+                }
+                .onTapGesture { coord in
                         if let coordinates = proxy.convert(coord, from: .local) {
                             showPopUp = coordinates
                         }
+                }
+                .overlay {
+                    VStack{
+                        Button("Show list") {
+                            showSheet = true
+                        }.padding(.horizontal, 16)
+                            .padding(.vertical, 8)
+                            .background(.white)
+                            .cornerRadius(16)
+                            .padding(16)
+                        Spacer()
                     }
+                }
             }
                 
                 if showPopUp != nil {
@@ -76,13 +91,47 @@ struct FavoritePlaces: View {
                         }, onDismissOutside: true, content: View)
                     }
                 }
-                
-            }
+            
+        }.sheet(isPresented: $showSheet, content: {
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(places) { place in
+                        let color = if place.favorite == true {
+                            Color.yellow.opacity(0.5)
+                        } else {
+                            Color.black.opacity(0.5)
+                        }
+                        VStack {
+                            Text(place.name).font(.title3).bold()
+                        }.frame(width: 150, height: 100).cornerRadius(16).overlay {
+                            RoundedRectangle(cornerRadius: 20).stroke(color.opacity(0.5), lineWidth: 1)
+                        }.shadow(radius: 5).padding(.horizontal, 8).padding(.top, 10)
+                            .onTapGesture {
+                                animateCamera(coordinates: place.coordinates)
+                                showSheet = false
+                            }
+                    }
+                }
+            }.presentationDetents(Set(height))
+        }).onAppear{
+            loadPlaces()
         }
+    }
+    
+    func animateCamera (coordinates: CLLocationCoordinate2D) {
+        withAnimation {
+            position = MapCameraPosition.region(MKCoordinateRegion(
+                center: coordinates,
+                span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+            )
+            )
+        }
+    }
     
     func savePlace(name: String, favorite: Bool, coordinates: CLLocationCoordinate2D) {
         let place = Place(name: name, coordinates: coordinates, favorite: favorite)
         places.append(place)
+        savePlaces()
     }
     
     func clearForm() {
@@ -90,6 +139,21 @@ struct FavoritePlaces: View {
         favorite = false
         error = false
         showPopUp = nil
+    }
+}
+
+extension FavoritePlaces {
+    func savePlaces() {
+        if let encodeData = try? JSONEncoder().encode(places) {
+            UserDefaults.standard.set(encodeData, forKey: "places")
+        }
+    }
+    
+    func loadPlaces() {
+        if let savedPlaces = UserDefaults.standard.data(forKey: "places"),
+           let decodedPlaces = try? JSONDecoder().decode([Place].self, from: savedPlaces) {
+               places = decodedPlaces
+           }
     }
 }
 
--- SwiftAppBundle/FavoritePlaces/model/Place.swift
+++ SwiftAppBundle/FavoritePlaces/model/Place.swift
@@ -8,9 +8,40 @@
 import Foundation
 import MapKit
 
-struct Place: Identifiable {
-    let id = UUID()
+struct Place: Identifiable, Codable {
+    var id = UUID()
     var name: String
     var coordinates: CLLocationCoordinate2D
     var favorite: Bool
+    
+    enum CodingKeys: CodingKey {
+        case id, name, favorite, latitude, longitude
+    }
+    
+    init(id: UUID = UUID(), name: String, coordinates: CLLocationCoordinate2D, favorite: Bool) {
+        self.id = id
+        self.name = name
+        self.coordinates = coordinates
+        self.favorite = favorite
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
+        let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
+        
+        coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        name = try container.decode(String.self, forKey: .name)
+        favorite = try container.decode(Bool.self, forKey: .favorite)
+        id = try container.decode(UUID.self, forKey: .id)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(coordinates.latitude, forKey: .latitude)
+        try container.encode(coordinates.longitude, forKey: .longitude)
+        try container.encode(name, forKey: .name)
+        try container.encode(favorite, forKey: .favorite)
+        try container.encode(id, forKey: .id)
+    }
 }
